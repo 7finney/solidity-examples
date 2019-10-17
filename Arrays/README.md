292520ab93b956c27cfbd1ebfa955bd12887d790
blue <soumyaniljana@localhost.localdomain>
2019-10-17 13:56:22 +0530

    modified with 1D and 3D examples

--- Arrays/README.md
+++ Arrays/README.md
@@ -2,17 +2,20 @@
 
 ## Simple array
 
-## Multidimensional 2D array
+## Multidimensional 2D and 3D array
 
 ### 1. Array Variable Declaration
 
-Solidity supports fixed sized and dynamic array declaration.
+Solidity supports fixed sized and dynamic array declaration and the dynamic 3D arrays will be declared same as the 2D arrays.
 
 ```Solidity
+uint[3] fixedSimpleArr;
+uint[] simpleDynamicArr;
 uint[3][3] fixedSized;   // fixed sized array
-uint[][3] arrayDynamic;  // dynamic array with each element an array with 3.
-uint[3][] dynamicArray;  // fixed sized array with each element a dynamic.
+uint[][3] arrayDynamic;  // dynamic array with each element an array with 3
+uint[3][] dynamicArray;  // fixed sized array with each element a dynamic
 uint[][] dynamicArr;    // dynamic array with dynamic array elements.
+uint[3][3][3] threeDArr;
 ```
 
 ### 2. Accessing array elements
@@ -27,12 +30,24 @@ But dynamic arrays can't be passed or return from an array. The error says:
 In order to use it, it is necessary to include `pragma experimental "ABIEncoderV2";`. Using `ABIEncoderV2` we would get warning saying "Do not use experimental features on live deployments".
 
 ```Solidity
+function setFixedSimpleArr(uint[3] memory simpleArr) public {
+  fixedSimpleArr = simpleArr;
+}
+
 function setFixedSized(uint[3][3] memory inputArr) public {
     fixedSized = inputArr;
 }
 
 function setDynamicArray(uint[3][] memory inputArr) public {
     dynamicArray = inputArr;
+}
+
+function setThreeDArr(uint[3][3][3] memory inputArr) public {
+    threeDArr = inputArr;
+}
+
+function getFixedSimpleArr(uint[3] memory simpleArr) public {
+  fixedSimpleArr = simpleArr;
 }
 
 function getFixedSized() public view returns (uint[3][3] memory) {
@@ -42,17 +57,28 @@ function getFixedSized() public view returns (uint[3][3] memory) {
 function getDynamicArray() public view returns (uint[3][] memory) {
     return dynamicArray;
 }
+function getThreeDArr() public view returns (uint[3][3][3] memory) {
+    return threeDArr;
+}
 ```
 
 With `ABIEncoderV2` we can access arrays like:
 
 ```Solidity
+ function setSimpleDynamicArr(uint[] memory simpleArr) public {
+   simpleDynamicArr = simpleArr;
+ }
+
 function setArrayDynamic(uint[][3] memory inputArr) public {
     arrayDynamic = inputArr;
 }
 
 function setDynamicArr(uint[][] memory inputArr) public {
     dynamicArr = inputArr;
+}
+
+function getSimpleDynamicArr(uint[] memory simpleArr) public {
+  simpleDynamicArr = simpleArr;
 }
 
 function getArrayDynamic() public view returns (uint[][3] memory) {
--- Arrays/arraySample.sol
+++ Arrays/arraySample.sol
@@ -1,13 +1,17 @@
 pragma solidity ^0.5.0;
 pragma experimental "ABIEncoderV2";
 
-contract Array {
+contract Arrays {
 
-    uint[3][3] public fixedSized;   // fixed sized array
+    uint[3] fixedSimpleArr;
+    uint[] simpleDynamicArr;
+    uint[3][3] fixedSized;   // fixed sized array
     uint[][3] arrayDynamic;  // dynamic array with each element an array with 3
     uint[3][] dynamicArray;  // fixed sized array with each element a dynamic
     uint[][] dynamicArr;    // dynamic array with dynamic array elemnts.
+    uint[3][3][3] threeDArr;
     constructor () public {
+      fixedSimpleArr = [1,2,3];
       uint j;
       for (uint i=0;i<3;i++){
           j = i+1;
@@ -18,9 +22,21 @@ contract Array {
       arrayDynamic[1] = [1,2,3,4,5,6,7,8];
       arrayDynamic[2] = temp;
       dynamicArray = new uint[2][](3);
-      dynamicArray[0] = [1,2];
-      dynamicArray[1] = [6,7];
-      dynamicArray[2] = [9,0];
+      dynamicArray = [[1,2], [6,7], [9,0]];
+      threeDArr = [
+          [[1,2,3], [4,5,6], [7,8,9]],
+          [[10,11,12], [13,14,15], [16,17,18]],
+          [[19,20,21], [22,23,24], [25,26,27]]
+      ];
+    }
+
+    function setFixedSimpleArr(uint[3] memory simpleArr) public {
+      fixedSimpleArr = simpleArr;
+    }
+
+   // This function is illegal
+    function setSimpleDynamicArr(uint[] memory simpleArr) public {
+      simpleDynamicArr = simpleArr;
     }
 
     function setFixedSized(uint[3][3] memory inputArr) public {
@@ -41,6 +57,18 @@ contract Array {
         dynamicArr = inputArr;
     }
 
+    function setThreeDArr(uint[3][3][3] memory inputArr) public {
+        threeDArr = inputArr;
+    }
+
+    function getFixedSimpleArr(uint[3] memory simpleArr) public {
+      fixedSimpleArr = simpleArr;
+    }
+
+    function getSimpleDynamicArr(uint[] memory simpleArr) public {
+      simpleDynamicArr = simpleArr;
+    }
+
     function getFixedSized() public view returns (uint[3][3] memory) {
         return fixedSized;
     }
@@ -59,19 +87,36 @@ contract Array {
         return dynamicArr;
     }
 
-    function getElementFixedSized(uint i, uint j) public view returns(uint) {
+    function getThreeDArr() public view returns (uint[3][3][3] memory) {
+        return threeDArr;
+    }
+
+    function getElementFixedSimpleArr() public view returns (uint[3] memory) {
+      return fixedSimpleArr;
+    }
+
+    // This function is illegal
+    function getElementSimpleDynamicArr() public view returns (uint[] memory) {
+      return simpleDynamicArr;
+    }
+
+    function getElementFixedSized(uint i, uint j) public view returns (uint) {
       return fixedSized[i][j];
     }
 
-    function getElementDynamicArray (uint i, uint j) public view returns(uint) {
+    function getElementDynamicArray(uint i, uint j) public view returns (uint) {
       return dynamicArray[i][j];
     }
 
-    function getElementArrayDynamic (uint i, uint j) public view returns(uint) {
+    function getElementArrayDynamic(uint i, uint j) public view returns (uint) {
       return arrayDynamic[i][j];
     }
 
-    function getElementDynamicArr (uint i, uint j) public view returns(uint) {
+    function getElementDynamicArr(uint i, uint j) public view returns (uint) {
       return dynamicArr[i][j];
+    }
+
+    function getElementThreeDArr(uint i, uint j, uint k) public view returns (uint) {
+        return threeDArr[i][j][k];
     }
 }
