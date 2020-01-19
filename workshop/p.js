9786acd12fe0600c7d1efe5ffab90a43ee78508b
VAKLONS <prasad.pexabyte@gmail.com>
2020-01-19 11:21:09 +0530

    new changes

--- workshop/p.js
+++ workshop/p.js
@@ -1,9 +1,11 @@
 var Web3 = require('web3');
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-console.log(web3.eth);
+// var web3 = Web3(Web3.HTTPProvider("http://localhost:8545"))
+var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+console.log(web3.eth.accounts);
+// console.log(web3.eth);
 
 var abi = '[{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"vault","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Deposit","type":"event"}]';
-var ClientReceipt = new web3.eth.Contract(JSON.parse(abi), "0x24a92Fa00D0e7391E357590e27e0135a9d7CF547");
+var ClientReceipt = web3.eth.contract(JSON.parse(abi));
 var clientReceipt = ClientReceipt.at("0x74bf166ba668678BF1Aa52787eacd3bB91676566");
 
 var event = clientReceipt.Deposit();
