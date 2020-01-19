b3f476ed5b705394664a96491215104c835f8596
VAKLONS <prasad.pexabyte@gmail.com>
2020-01-19 19:15:44 +0530

    adding complex addition

--- workshop/p.js
+++ workshop/p.js
@@ -1,11 +1,38 @@
-var Web3 = require('web3');
+const Web3 = require('web3');
+const solc = require('solc');
+const fs = require('fs');
+const path = require('path');
 // var web3 = Web3(Web3.HTTPProvider("http://localhost:8545"))
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-web3.eth.getAccounts().then(console.log);
+const p = path.resolve(__dirname, "reciept.sol");
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// web3.eth.getAccounts().then(console.log);
+const source = fs.readFileSync(p, 'UTF-8');
+const input = {
+    language: 'Solidity',
+    sources: {
+      'reciept.sol': {
+        content: source
+      }
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*']
+        }
+      }
+    }
+  };
+const pbi = JSON.parse(solc.compile(JSON.stringify(input)));
 
-var abi = '[{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"vault","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Deposit","type":"event"}]';
-var ClientReceipt = new web3.eth.Contract(JSON.parse(abi), "0xcB96c4147583A0aAfEAf694c91b23941F69880E5");
-ClientReceipt.methods.deposit().send({ from: "0x649C5Af6056aCEd3eA358412A79B3b30c39019C6" }).then((result) => {
-    if (result)
+const abi = pbi.contracts['reciept.sol'].ClientReceipt.abi;
+const ClientReceipt = new web3.eth.Contract(abi, "0x989CD4D7a3374EEf60f038027C22cf876E27f066");
+ClientReceipt.methods.deposit("0x7e5605F81c51B813089D19950FF087136a1E7cBF").send({ from: "0x7e5605F81c51B813089D19950FF087136a1E7cBF" }).then((result) => {
+    if (result){}
         console.log("result:", result);
 });
+
+web3.eth.getAccounts().then(e => {
+  web3.eth.getBalance(e);
+});
+
+// console.log("0th acc.", )
